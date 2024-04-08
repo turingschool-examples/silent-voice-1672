44bae27ef971fd78ecba2d60ed96cdc6876fdd32
NeilTheSeal <neil.hendren@gmail.com>
2024-04-08 11:58:39 -0600

    Improve sad path error messages

--- app/controllers/movie_actors_controller.rb
+++ app/controllers/movie_actors_controller.rb
@@ -2,7 +2,9 @@ class MovieActorsController < ApplicationController
   def create
     movie_actor = MovieActor.new(create_movie_actor_params)
 
-    flash[:alert] = "Error: invalid input" unless movie_actor.save
+    unless movie_actor.save
+      flash[:alert] = "Error: #{movie_actor.errors.full_messages}"
+    end
 
     redirect_to("/movies/#{params[:movie_id]}")
   end
