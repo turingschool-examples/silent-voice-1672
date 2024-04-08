2b4b46c3dfc655ffd89bc2a4f6fa243d954b1b3b
NeilTheSeal <neil.hendren@gmail.com>
2024-04-08 11:55:50 -0600

    Fix typo

--- app/controllers/movie_actors_controller.rb
+++ app/controllers/movie_actors_controller.rb
@@ -2,7 +2,7 @@ class MovieActorsController < ApplicationController
   def create
     movie_actor = MovieActor.new(create_movie_actor_params)
 
-    flash[:alert] = "Error: invalid input" if movie_actor.save
+    flash[:alert] = "Error: invalid input" unless movie_actor.save
 
     redirect_to("/movies/#{params[:movie_id]}")
   end
