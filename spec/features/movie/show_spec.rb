609c5140eb9d2e4093c4feea68351b31ce95b2c1
Billy Wallace <wallace.billy1@gmail.com>
2024-04-08 11:07:24 -0600

    Fix formatting

--- spec/features/movie/show_spec.rb
+++ spec/features/movie/show_spec.rb
@@ -74,16 +74,6 @@ RSpec.describe "the movie show" do
     end 
   end
 
-  # As a user,
-  # When I visit a movie show page,
-  # I do not see any actors listed that are not part of the movie
-  # And I see a form to add an actor to this movie
-  # When I fill in the form with the ID of an actor that exists in the database
-  # And I click submit
-  # Then I am redirected back to that movie's show page
-  # And I see the actor's name is now listed
-  # (You do not have to test for a sad path, for example if the id submitted is not an existing actor)
-
   it "displays a form to add an actor to this movie" do
     visit "/movies/#{@movie1.id}"
 
