3b28999d9f8337dc62f5bb56e65ce3f20803af6f
Grant Davis <grantdavis303@gmail.com>
2024-04-08 09:12:45 -0600

    Update Spec File

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -31,6 +31,7 @@ begin
 rescue ActiveRecord::PendingMigrationError => e
   abort e.to_s.strip
 end
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
@@ -63,9 +64,14 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
+
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
     with.test_framework :rspec
     with.library :rails
   end
 end
+
+RSpec.configure do |config|
+  config.formatter = :documentation
+end 
